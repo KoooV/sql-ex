2a33486289191061ba7630d63f99e5985b77edfc
KoV <alexkovalev13681@gmail.com>
2025-06-27 18:49:57 +0300

    add 9 -14

--- sqlex.sql
+++ sqlex.sql
@@ -1,18 +1,18 @@
 -- 1 задание
 SELECT model,speed,hd FROM PC 
-WHERE price < 500;
+WHERE price < 500
 
 -- 2 задание
 SELECT DISTINCT maker FROM Product
-WHERE type = 'Printer';
+WHERE type = 'Printer'
 
 -- 3 задание
 SELECT l.model, l.ram, l.screen FROM Laptop l
-WHERE price > 1000;
+WHERE price > 1000
 
 -- 4 задание
 SELECT * FROM Printer
-WHERE color = 'y'; 
+WHERE color = 'y'
 
 -- 5 задание
 SELECT p.model, p.speed, p.hd FROM PC p
@@ -22,7 +22,7 @@ WHERE (cd = '12x' OR cd = '24x') AND price < 600
 SELECT DISTINCT p.maker, l.speed
 FROM Product p
 JOIN Laptop l ON l.model = p.model
-WHERE l.hd >= 10;
+WHERE l.hd >= 10
 
 -- 7 задание
 SELECT DISTINCT pr.model, pc.price FROM Product pr
@@ -43,4 +43,31 @@ SELECT DISTINCT p.maker
 FROM Product p
 WHERE p.type = 'PC' AND p.maker NOT IN(
 	SELECT p.maker FROM Product p
-	WHERE p.type = 'Laptop');
+	WHERE p.type = 'Laptop')
+	
+-- 9 задание
+SELECT DISTINCT pr.maker FROM Product pr
+JOIN PC p ON p.speed >= 450 AND p.model = pr.model
+
+-- 10 задание
+SELECT p.model, p.price FROM Printer p
+WHERE p.price = (SELECT MAX(p.price) FROM Printer p)
+
+-- 11 задания
+SELECT AVG(p.speed) FROM PC p
+
+-- 12 задания
+SELECT AVG(p.speed) FROM Laptop p
+WHERE p.price > 1000
+
+-- 13 задание
+SELECT DISTINCT AVG(p.speed) FROM PC p
+JOIN Product pr ON pr.maker = 'A' AND pr.model = p.model
+
+-- 14 задание
+SELECT s.class, s.name, c.country FROM Ships s
+JOIN Classes c ON c.class = s.class AND c.numGuns >= 10
+
+
+
+
